59757ecd89ca63c236db60b89e25470ba38f2cde
Richard Sproson <rsproson@tradeprint.co.uk>
2018-10-14 10:39:05 +0100

    Tests for generating primes and generating primes. Also displaying the prime table in the UI

--- src/client/app/primes.jsx
+++ src/client/app/primes.jsx
@@ -18,14 +18,53 @@ export default class Primes extends React.Component {
         this.state = {
             heading: "Generate a prime table",
             primeNumbers: [],
-            primesToGenerate: 0
+            primesToGenerate: ""
         };
 
         // Setup any hard variables not required in the DOM
         this.maxNumbersToLoop = 1000; // If array is bigger than this it will split for performance
 
         // Setup all functions requiring use of this
+        this.generatePrimes = this.generatePrimes.bind(this);
+        this.loopUntilPrimesToGenerate = this.loopUntilPrimesToGenerate.bind(this);
         this.updateState = this.updateState.bind(this);
+    }
+
+    generatePrimes() {
+        const primesToGenerate = Number(this.state.primesToGenerate);
+
+        this.loopUntilPrimesToGenerate(primesToGenerate).then(response => {
+            this.setState({primeNumbers: response});
+        });
+    }
+
+    loopUntilPrimesToGenerate(primesToGenerate) {
+        return new Promise((resolve) => {
+            let i = 2;
+            let primeNumbers = [];
+            while (primeNumbers.length < primesToGenerate) {
+                if (this.constructor.isPrime(i)) {
+                    primeNumbers.push(i);
+                }
+
+                if (primesToGenerate === primeNumbers.length) {
+                    resolve(primeNumbers);
+                }
+                i++
+            }
+        });
+    }
+
+    static isPrime(num) {
+        if (num === 0 || num === 1) {
+            return false;
+        }
+        for (let i = 2; i < num; i++) {
+            if (num % i === 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     render() {
@@ -42,18 +81,41 @@ export default class Primes extends React.Component {
                         updateParent={this.updateState}
                     />
                 </div>
-                <table className="table table-striped table-responsive">
-                    <thead>
+                <div className={"col-md-6 col-xs-12"}>
+                    <button onClick={this.generatePrimes} className={"btn btn-primary"} disabled={this.state.primesToGenerate === ("" || 0)}>
+                        Generate Prime Table
+                    </button>
+                </div>
+                { this.state.primeNumbers.length ?
+                    <table className="table table-striped table-responsive">
+                        <thead>
                         <tr>
                             <th> </th>
+                            {this.state.primeNumbers.map((number, i) => {
+                                return (
+                                    <th key={'heading' + i}>{number}</th>
+                                )
+                            })}
                         </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td> </td>
-                        </tr>
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                        {this.state.primeNumbers.map((primeNumberRow, j) => {
+                            return (
+                                <tr key={'primeNumberRow' + j}>
+                                    <th>{primeNumberRow}</th>
+
+                                    {this.state.primeNumbers.map((primeNumberColumn, k) => {
+                                        return (
+                                            <td key={'primeNumberRow' + j + 'primeNumberColumn' + k}>{primeNumberRow * primeNumberColumn}</td>
+                                        )
+                                    })}
+                                </tr>
+                            )
+                        })}
+                        </tbody>
+                    </table>
+                    :
+                    null }
             </div>
         )
     }
--- src/client/tests/primes.test.js
+++ src/client/tests/primes.test.js
@@ -10,8 +10,27 @@ import Primes from './../app/primes.jsx';
 
 
 describe('<Primes /> component', function () {
+    let wrapper;
+    let instance;
     it('Primes renders without crashing', function () {
-        shallow(<Primes
-        />);
+        wrapper = shallow(<Primes />);
+        instance = wrapper.instance();
+    });
+
+    it('Check if is prime number', function () {
+        expect(instance.constructor.isPrime(3)).toEqual(true);
+        expect(instance.constructor.isPrime(7)).toEqual(true);
+        expect(instance.constructor.isPrime(10)).toEqual(false);
+        expect(instance.constructor.isPrime(0)).toEqual(false);
+        expect(instance.constructor.isPrime(1)).toEqual(false);
+    });
+
+    it('Should expect promise to resolve', function () {
+        const expectedArray = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
+        return instance.loopUntilPrimesToGenerate(10)
+            .then(response => {
+                expect(response).toHaveLength(10);
+                expect(response).toEqual(expectedArray);
+            })
     });
 });
