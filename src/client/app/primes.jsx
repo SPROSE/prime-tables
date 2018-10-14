502a3073a4bc0e5f51275c7a09b86fc5f62e6008
Richard Sproson <rsproson@tradeprint.co.uk>
2018-10-14 11:47:17 +0100

    Performance tuning the generate primes algorithm. Changing the DOM. Adding a loader message.

--- src/client/app/primes.jsx
+++ src/client/app/primes.jsx
@@ -18,23 +18,26 @@ export default class Primes extends React.Component {
         this.state = {
             heading: "Generate a prime table",
             primeNumbers: [],
-            primesToGenerate: ""
+            primeTable: [],
+            primesToGenerate: "",
+            loading: false
         };
-
-        // Setup any hard variables not required in the DOM
-        this.maxNumbersToLoop = 1000; // If array is bigger than this it will split for performance
 
         // Setup all functions requiring use of this
         this.generatePrimes = this.generatePrimes.bind(this);
+        this.generatePrimeTable = this.generatePrimeTable.bind(this);
         this.loopUntilPrimesToGenerate = this.loopUntilPrimesToGenerate.bind(this);
         this.updateState = this.updateState.bind(this);
     }
 
     generatePrimes() {
         const primesToGenerate = Number(this.state.primesToGenerate);
+        this.setState({
+            loading: true
+        });
 
         this.loopUntilPrimesToGenerate(primesToGenerate).then(response => {
-            this.setState({primeNumbers: response});
+            this.generatePrimeTable(response);
         });
     }
 
@@ -42,11 +45,28 @@ export default class Primes extends React.Component {
         return new Promise((resolve) => {
             let i = 2;
             let primeNumbers = [];
+            // set map to store values not prime to help with prime checks
+            let markedNotPrimeMap = new Map();
+
             while (primeNumbers.length < primesToGenerate) {
-                if (this.constructor.isPrime(i)) {
+                if (!(markedNotPrimeMap.has(i))) {
+                    markedNotPrimeMap.set(i**2, [i]);
                     primeNumbers.push(i);
+                } else {
+                    let primes = markedNotPrimeMap.get(i);
+                    primes.forEach(prime=> {
+                        let nextMultipleOfPrime = prime + i;
+                        if (markedNotPrimeMap.has(nextMultipleOfPrime)) {
+                            markedNotPrimeMap.get(nextMultipleOfPrime).push(prime);
+                        } else {
+                            markedNotPrimeMap.set(nextMultipleOfPrime, [prime]);
+                        }
+                    });
+                    // Delete as we pass these values to keep the list low
+                    markedNotPrimeMap.delete(i);
                 }
 
+                // Once we hit the number of primes required, resolve
                 if (primesToGenerate === primeNumbers.length) {
                     resolve(primeNumbers);
                 }
@@ -55,67 +75,85 @@ export default class Primes extends React.Component {
         });
     }
 
-    static isPrime(num) {
-        if (num === 0 || num === 1) {
-            return false;
-        }
-        for (let i = 2; i < num; i++) {
-            if (num % i === 0) {
-                return false;
-            }
-        }
-        return true;
+    generatePrimeTable(primes) {
+        let primeTable = [];
+
+        primes.map((primeNumberRow) => {
+            let row = [];
+            row.push(primeNumberRow);
+            primes.map((primeNumberColumn) => {
+                row.push(primeNumberRow * primeNumberColumn);
+            });
+            primeTable.push(row);
+        });
+        this.setState({
+            primeNumbers: primes,
+            primeTable: primeTable,
+            loading: false
+        });
     }
 
     render() {
         return (
             <div>
                 <h1>{this.state.heading}</h1>
-                <div className={"col-md-6 col-xs-12"}>
-                    <TextInput
-                        required={true}
-                        value={this.state.primesToGenerate}
-                        label={"Enter number of primes to generate"}
-                        inputType={"number"}
-                        placeholder={"Enter a numeric value"}
-                        updateParent={this.updateState}
-                    />
+                <div className={"row"}>
+                    <div className={"col-md-6 col-xs-12"}>
+                        <TextInput
+                            required={true}
+                            value={this.state.primesToGenerate}
+                            label={"Enter number of primes to generate"}
+                            inputType={"number"}
+                            placeholder={"Enter a numeric value"}
+                            updateParent={this.updateState}
+                        />
+                    </div>
+                    <div className={"col-md-6 col-xs-12"}>
+                        <button onClick={this.generatePrimes} className={"btn btn-primary"} disabled={this.state.primesToGenerate === ("" || 0)}>
+                            Generate Prime Table
+                        </button>
+                    </div>
                 </div>
-                <div className={"col-md-6 col-xs-12"}>
-                    <button onClick={this.generatePrimes} className={"btn btn-primary"} disabled={this.state.primesToGenerate === ("" || 0)}>
-                        Generate Prime Table
-                    </button>
-                </div>
-                { this.state.primeNumbers.length ?
-                    <table className="table table-striped table-responsive">
-                        <thead>
-                        <tr>
-                            <th> </th>
-                            {this.state.primeNumbers.map((number, i) => {
+                <div className={"row"}>
+                { !this.state.loading ?
+                    <div className="col-xs-12 table-responsive">
+                        <table className="table table-striped">
+                            <thead>
+                            <tr>
+                                <th> </th>
+                                {this.state.primeNumbers.map((number, i) => {
+                                    return (
+                                        <th key={'heading' + i}>{number}</th>
+                                    )
+                                })}
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {this.state.primeTable.map((primeNumberRow, j) => {
                                 return (
-                                    <th key={'heading' + i}>{number}</th>
+                                    <tr key={'primeNumberRow' + j}>
+                                        {this.state.primeTable[j].map((primeNumberColumn, k) => {
+                                            if (k === 0) {
+                                                return (
+                                                    <th key={'primeNumberRow' + j + 'primeNumberColumn' + k}>{primeNumberColumn}</th>
+                                                )
+                                            }
+                                            else {
+                                                return (
+                                                    <td key={'primeNumberRow' + j + 'primeNumberColumn' + k}>{primeNumberColumn}</td>
+                                                )
+                                            }
+                                        })}
+                                    </tr>
                                 )
                             })}
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {this.state.primeNumbers.map((primeNumberRow, j) => {
-                            return (
-                                <tr key={'primeNumberRow' + j}>
-                                    <th>{primeNumberRow}</th>
-
-                                    {this.state.primeNumbers.map((primeNumberColumn, k) => {
-                                        return (
-                                            <td key={'primeNumberRow' + j + 'primeNumberColumn' + k}>{primeNumberRow * primeNumberColumn}</td>
-                                        )
-                                    })}
-                                </tr>
-                            )
-                        })}
-                        </tbody>
-                    </table>
+                            </tbody>
+                        </table>
+                    </div>
                     :
-                    null }
+                    <p>Loading table...</p>
+                }
+                </div>
             </div>
         )
     }
